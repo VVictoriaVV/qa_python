3e8bfbf2ca31e3cf2a79d811738aebad8272e4f1
Viktoria <viktoria.loparevich@gmail.com>
2023-04-14 23:03:13 +0300

    New commit

--- tests.py
+++ tests.py
@@ -22,3 +22,51 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_book_rating_book_doesnt_exist_got_rating_is_none(self):
+        collector = BooksCollector()
+        assert collector.get_book_rating('Такой книги нет') is None
+
+    def test_set_book_rating_rating_0_book_rating_is_1(self):
+        collector = BooksCollector()
+        collector.set_book_rating('Гордость и предубеждение и зомби', 10)
+        assert collector.get_book_rating('Гордость и предубеждение и зомби') !=11
+
+
+    def test_get_books_with_specific_rating_rating_doesnt_exist_return_empty_list(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гарри Поттер')
+        assert len(collector.get_books_with_specific_rating(3)) == 0
+
+
+    def test_get_list_of_favorites_books_empty_favorites_books_list_return_empty_list(self):
+
+        collector = BooksCollector()
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_add_book_in_favorites_add_book_in_favorites_return_one_book(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Мертвые души')
+        collector.add_book_in_favorites('Мертвые души')
+        assert collector.get_list_of_favorites_books() == ['Мертвые души']
+
+    def test_add_book_in_favorites_add_nonexistent_book_favorite_books_list_empty(self):
+
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Ревизор')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+
+    def test_delete_book_from_favorites_add_and_delete_book_from_favorites_empty_favorite_list(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Вася')
+        collector.add_book_in_favorites('Вася')
+        collector.delete_book_from_favorites('Вася')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    def test_delete_book_from_favorites_delete_no_exists_book_from_favorites_empty_favorite_list(self):
+        collector = BooksCollector()
+        collector.delete_book_from_favorites('Вася')
+        assert len(collector.get_list_of_favorites_books()) == 0
